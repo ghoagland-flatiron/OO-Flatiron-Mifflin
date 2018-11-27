1e9fe6ccaed0afe4fd2af9f90c37001bd48f8caf
Eric Kim <hyeokjungkim@gmail.com>
2018-11-27 12:51:56 -0500

    solution

--- run.rb
+++ run.rb
@@ -4,7 +4,12 @@ require 'pry'
 
 
 #Test your code here
+julissa = Manager.new("Julissa", "Flatiron", 50)
+roman = Manager.new("Roman", "Roman Empire", 51)
 
-
+eric = Employee.new("Eric", 10000, roman)
+joe = Employee.new("Joe", 10500, roman)
+julissa.hire_employee("Kiyana", 12000)
+johnmark = Employee.new("John Mark", 10500, julissa)
 binding.pry
 puts "done"
